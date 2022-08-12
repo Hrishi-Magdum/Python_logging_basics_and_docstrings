84b4388b90aaee3cf9a23f9eae82e7de7986386b
IamHrishi <magdum.hrishi@gmail.com>
2022-08-12 20:28:28 +0530

    or model updated

--- or.py
+++ or.py
@@ -10,21 +10,21 @@ def main(data, modelName, plotName, eta, epochs):
     X,y = prepare_data(df_OR)
 
     # Initializing class Perceptron
-    model_or = Perceptron(eta=eta, epochs=epochs)
-    model_or.fit(X,y)
+    model = Perceptron(eta=eta, epochs=epochs)
+    model.fit(X,y)
 
-    _ = model_or.total_loss
+    _ = model.total_loss
 
-    model_or.save(filename=modelName, model_dir="model")
+    model.save(filename=modelName, model_dir="model")
 
     # saving the plot
-    save_plot(df_OR, model_or, filename=plotName)
+    save_plot(df_OR, model, filename=plotName)
 
 if __name__ == "__main__":
     # data for OR gate
     OR = {'x1':[0,0,1,1], 
         'x2' : [0,1,0,1], 
-        'y':[0,0,0,1]}
+        'y':[0,1,1,1]}
     ETA = 0.3
     EPOCHS = 10
     main(data=OR, modelName='or.model', plotName='or.png',eta=ETA, epochs=EPOCHS)
